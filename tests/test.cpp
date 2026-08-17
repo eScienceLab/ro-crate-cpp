c2be7ab4c2b9ae09b414ab9d282ee1ed7e95d18c
Oliver Woolland <oliver.woolland@manchester.ac.uk>
2026-08-17 11:55:00 +0100

    Improve consistency of assertions

--- tests/test.cpp
+++ tests/test.cpp
@@ -14,25 +14,26 @@ TEST_CASE("Produce an example RO-Crate faithfully")
     // Add description to the root metadata entity (ro-crate-metadata.json) 
     Entity root = crate.getEntity("ro-crate-metadata.json");
     root.set("description", "RO-Crate Metadata File Descriptor (this file)");
-
     REQUIRE_NOTHROW(crate.getEntity("ro-crate-metadata.json"));
-    REQUIRE_NOTHROW(crate.getEntity("./"));
 
     // Add name, description to the root data entity (./)
     Entity rootData = crate.getEntity("./");
     rootData.set("name", "Example RO-Crate");
     rootData.set("description", "The RO-Crate Root Data Entity");
+    REQUIRE_NOTHROW(crate.getEntity("./"));
 
     // Create the person
     Entity alice({"Person"});
     alice.set("name", "Alice");
     alice.set("description", "One of hopefully many Contextual Entities");
     crate.addEntity("#alice", alice);
+    REQUIRE_NOTHROW(crate.getEntity("#alice"));
 
     // Create the place
     Entity catalinaPark({"Place"});
     catalinaPark.set("name", "Catalina Park");
     crate.addEntity("http://sws.geonames.org/8152662/", catalinaPark);
+    REQUIRE_NOTHROW(crate.getEntity("http://sws.geonames.org/8152662/"));
 
     // Create two datasets
     Entity data1({"File"});
@@ -40,10 +41,9 @@ TEST_CASE("Produce an example RO-Crate faithfully")
     data1.set("author", alice);
     data1.set("contentLocation", catalinaPark);
     crate.addEntity("data1.txt", data1);
+    REQUIRE_NOTHROW(crate.getEntity("data1.txt"));
 
     Entity data2({"File"});
-    REQUIRE_NOTHROW(crate.addEntity("data2.txt", data2));
-}
     crate.addEntity("data2.txt", data2);
     REQUIRE_NOTHROW(crate.getEntity("data2.txt"));
 }
