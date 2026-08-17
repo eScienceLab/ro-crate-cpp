2e962f1fb5d1e24e66f7214e0ba2f5714f48a581
Oliver Woolland <oliver.woolland@manchester.ac.uk>
2026-08-17 13:14:13 +0100

    Reorder and add basic unit tests

--- tests/unit.cpp
+++ tests/unit.cpp
@@ -6,8 +6,26 @@
 
 using rocrate::Entity;
 
-TEST_CASE("Ensure failure if entity is linked before being assigned an ID", "[unit]")
-{
+// ------------------------------------------------------------------------------
+// Unit tests for Entity class
+// ------------------------------------------------------------------------------
+
+TEST_CASE("Ensure entity creation occurs without throwing", "[unit]") {
+    Entity alice({"Person"});
+}
+
+TEST_CASE("Ensure entity creation throws if no types are provided", "[unit]") {
+  // Create an entity with an empty types vector, which should throw an invalid_argument exception
+  REQUIRE_THROWS_AS(Entity(std::vector<std::string>{}), std::invalid_argument);
+}
+
+TEST_CASE("Ensure set by value succeeds with valid input", "[unit]") {
+    Entity alice({"Person"});
+    REQUIRE_NOTHROW(alice.set("name", "Alice"));
+    REQUIRE_NOTHROW(alice.set("description", "One of hopefully many Contextual Entities"));
+}
+
+TEST_CASE("Ensure failure if entity is linked before being assigned an ID", "[unit]") {
     // Create the person, but do not add to a crate (so no @id yet)
     Entity alice({"Person"});
     alice.set("name", "Alice");
@@ -19,4 +37,42 @@ TEST_CASE("Ensure failure if entity is linked before being assigned an ID", "[un
 
     // This fails at link time because the linked entity has no @id yet.
     REQUIRE_THROWS_AS(data1.set("author", alice), std::runtime_error);
+}
+
+TEST_CASE("Ensure set by entity succeeds with valid input", "[unit]") {
+  // This test is exercised by the integration test as it requires a crate
+  SUCCEED("");
+}
+
+TEST_CASE("Add entity rejects entities with duplicate IDs", "[unit]") {
+  // Create a crate and add an entity
+  rocrate::ROCrate crate;
+
+  Entity alice({"Person"});
+  alice.set("name", "Alice");
+  crate.addEntity("#alice", alice);
+
+  // Attempt to add another entity with the same ID
+  Entity aliceDuplicate({"Person"});
+  aliceDuplicate.set("name", "Alice Duplicate");
+  REQUIRE_THROWS_AS(crate.addEntity("#alice", aliceDuplicate), std::runtime_error);
+}
+
+TEST_CASE("Get entity throws if entity not found", "[unit]") {
+  // Create a crate 
+  rocrate::ROCrate crate;
+
+  // Attempt to get an entity that does not exist
+  REQUIRE_THROWS_AS(crate.getEntity("#bob"), std::runtime_error);
+}
+
+TEST_CASE("RO-Crate initilises with valid root metadata and dataset entities", "[unit]") {
+  // Create a crate
+  rocrate::ROCrate crate;
+  
+  // Check that the root metadata entity exists
+  REQUIRE_NOTHROW(crate.getEntity("ro-crate-metadata.json"));
+  
+  // Check that the root dataset entity exists
+  REQUIRE_NOTHROW(crate.getEntity("./"));
 }
